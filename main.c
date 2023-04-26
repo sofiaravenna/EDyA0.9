212e4bbe363dc892b21492e9a21466225935eb2b
Sofía <ravennasofia03@gmail.com>
2023-04-25 21:57:04 -0300

    :(

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 //Defina una función que calcule la longitud de una cadena de caracteres.
 #include <stdio.h>
+void longitud(char *);
 
 int main() {
-    printf("Hello, World!\n");
+
+    char x[1000];
+    char *px=&x;
+    printf("Ingresar una cadena de caracteres: ");
+    fflush(stdin);
+    gets(x);
+    longitud(&x);
     return 0;
+}
+
+void longitud(char *px){
+    int cont=0;
+    for(int i=0; *px[i] != '\0'; i++) {
+        cont = +;
+    }
+    printf("\nEl texto ingresado tiene %d caracteres.",cont);
 }
